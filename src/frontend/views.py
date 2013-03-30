a81b7f315b02bbd412d12dec1ced6e886879f2a2
Christopher Troup <chris@sheepdoginc.ca>
2013-03-30 16:16:39 -0300

    Allow team selection

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render_to_response
 from django.db.models.aggregates import Count
 
 def kickoff(request):
-    N = 10000
+    N = 50000
     simulator = PlayoffSimulator()
     
     for team in simulator.east_points:
@@ -21,11 +21,22 @@ def kickoff(request):
 
 
 def show_results(request):
-    simulation = Simulation.objects.annotate(game_results_count=Count('gameresult')).filter(my_team='TOR', game_results_count__gt=1).order_by('-run_at')[0]
+    
+    if 'team' in request.GET:
+        my_team = request.GET['team']
+    else:
+        my_team = 'TOR'
+    
+    simulation = Simulation.objects.annotate(game_results_count=Count('gameresult')).filter(my_team=my_team, game_results_count__gt=1).order_by('-run_at')[0]
     game_results = GameResult.objects.filter(simulation=simulation).order_by('date')
+    
+    teams = Simulation.objects.order_by('-run_at').values_list('my_team')[:30]
+    teams = sorted([x[0] for x in teams])
     
     return render_to_response('results.html',
                               {'game_results': game_results,
-                               'simulation': simulation})
+                               'simulation': simulation,
+                               'teams': teams,
+                               'my_team': my_team})
     
     
